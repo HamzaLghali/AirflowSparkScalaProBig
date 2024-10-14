93ed11b77d01c098036b8dd3a9d95a09f52c7f1c
HamzaLg <92524604+HamzaLghali@users.noreply.github.com>
2024-10-14 12:06:34 +0100

    importing data from cvs to postgres working good

---
+++ airflow/jobs/BigDataProjDeltaScala/src/main/scala/tools/PostgreSqlSink.scala
@@ -0,0 +1,32 @@
+package tools
+
+
+import java.sql._
+
+class PostgreSqlSink(url: String, user: String, pwd: String) extends org.apache.spark.sql.ForeachWriter[org.apache.spark.sql.Row] {
+  val driver = "org.postgresql.Driver"
+  var connection: java.sql.Connection = _
+  var statement: java.sql.PreparedStatement = _
+
+  val v_sql = "insert INTO test(id, datecre) values(1,'2024-10-08')"
+
+  def open(partitionId: Long, version: Long): Boolean = {
+    Class.forName(driver)
+    connection = java.sql.DriverManager.getConnection(url, user, pwd)
+    connection.setAutoCommit(false)
+    statement = connection.prepareStatement(v_sql)
+    true
+  }
+
+  def process(value: org.apache.spark.sql.Row): Unit = {
+    // ignoring value(0) as this is address
+    statement.setString(1, value(1).toString)
+    statement.setString(2, value(2).toString)
+    statement.executeUpdate()
+  }
+
+  def close(errorOrNull: Throwable): Unit = {
+    connection.commit()
+    connection.close
+  }
+}
