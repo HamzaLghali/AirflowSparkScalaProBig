482c9793e09031319aa36f93b18c24ed83f596ce
HamzaLg <92524604+HamzaLghali@users.noreply.github.com>
2024-10-16 12:17:15 +0100

    json handling v3 top

---
+++ airflow/jobs/BigDataProjDeltaScala/src/main/scala/com/RandomUser.scala
@@ -0,0 +1,32 @@
+package com
+
+import org.json4s.DefaultFormats
+import org.json4s.native.JsonMethods.parse
+import scalaj.http.{Http, HttpResponse}
+
+object RandomUser extends App{
+
+
+
+  implicit val formats = DefaultFormats
+  val response: HttpResponse[String] = Http("https://dummyjson.com/users").asString
+  val json = parse(response.body)
+
+  val seq =Seq("id","firstName","lastName","maidenName","age","gender","email","phone","username","password","birthDate","image","bloodGroup","height","weight","eyeColor","ip","macAddress","university" )
+  var i : Int = 0
+  var j: Int = 0
+
+  while (j<2){
+    val firstResult = (json \ "users")(j)
+
+      for (field <- seq) {
+          val value = (firstResult \ field)
+
+          // Try to extract as String or Int
+          val extractedValue = value.extractOpt[String].getOrElse(value.extractOpt[Int].getOrElse("Unknown"))
+          println(s"$field: $extractedValue")
+        }
+      j=j+1
+  }
+}
+
