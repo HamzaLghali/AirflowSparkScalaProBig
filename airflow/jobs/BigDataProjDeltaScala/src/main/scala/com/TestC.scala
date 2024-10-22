52f1be0bc9191dc60f2b9678bfde4431aea4e2a2
HamzaLg <92524604+HamzaLghali@users.noreply.github.com>
2024-10-22 16:21:47 +0100

    docker refa

--- airflow/jobs/BigDataProjDeltaScala/src/main/scala/com/TestC.scala
+++ airflow/jobs/BigDataProjDeltaScala/src/main/scala/com/TestC.scala
@@ -1,48 +1,31 @@
 package com
 
-import org.apache.spark.sql.{DataFrame, SaveMode}
-import tools.PostgresConnection.{password, url, username}
-import tools.TechTools.loadTable
+import tools.PostgresConnection.statement
+import tools.SparkCore.spark
+
 
 object TestC extends App {
 
 
+  val csvD = spark.read.option("header", "true").csv("src/main/resources/data/BankChurners.csv")
 
- val benemob = loadTable("benemob")
-  val beneag = loadTable("beneage")
+  csvD.printSchema()
+
+
+  val seqlenght = csvD.schema.fieldNames.toSeq
 
 
 
-  val dtmerge= benemob.union(beneag).distinct().withColumnRenamed("account_number", "account_number_bene")
+  var i=0
+  for ( f <- seqlenght-2){
 
-  val ops= loadTable("operations")
-  val dfjoi = dtmerge.join( ops, dtmerge.col("account_number_bene") === ops.col("account_number"),"outer")
-    .select("account_number_bene","name", "address","phone_number","email","operation_id","operation_type","amount","operation_date","description")
-    .sort("operation_id")
-
-  val countdj= dfjoi.count()
-val counbene= dtmerge.count()
-
-  val counop= loadTable("operations").count()
-  print(s"ops: $counop ,bene: $counbene ,all: $countdj")
-
-  dfjoi.show()
-
-  def saveToPostgres(df: DataFrame, tableName: String): Unit = {
-    df.write
-      .format("jdbc")
-      .option("url", url)
-      .option("dbtable", tableName)
-      .option("user", username)
-      .option("password", password)
-      .option("driver", "org.postgresql.Driver")
-      .mode(SaveMode.Overwrite)
-      .save()
+    println(seqlenght(i).isInstanceOf[String])
+    i=i+1
+    //val st =statement.executeQuery(s"create table")
   }
 
-  saveToPostgres(dtmerge, "allbene")
 
-  val allbene =loadTable("allbene")
-  //allbene.show()
+
+
 
 }
